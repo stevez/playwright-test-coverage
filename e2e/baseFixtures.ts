3f8211e06c909029d9b27419aa5602e9a321938c
Steve Zhang <huigang.zhang@gmail.com>
2022-04-08 21:57:27 -0400

    added the vite-plugin-istanbul for e2e test

--- e2e/baseFixtures.ts
+++ e2e/baseFixtures.ts
@@ -1,51 +1,32 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as crypto from 'crypto';
-import { test as baseTest } from '@playwright/test'
-import v8toIstanbul from 'v8-to-istanbul'
+import { test as baseTest } from '@playwright/test';
 
-const c8CLIOutput = path.join(process.cwd(), 'coverage/tmp');
 const istanbulCLIOutput = path.join(process.cwd(), '.nyc_output');
+
 export function generateUUID(): string {
   return crypto.randomBytes(16).toString('hex');
 }
 
 export const test = baseTest.extend({
-  page: async ({page, baseURL}, use) => {
-
-    await fs.promises.mkdir(c8CLIOutput, { recursive: true });
-
-    await page.coverage.startJSCoverage({
-      resetOnNavigation: false,
-    });
-
-    await use(page);
-
-    //write v8 coverage file
-    let coverage = await page.coverage.stopJSCoverage();
-    coverage = coverage.filter((it) => it.url.match(/(?<=\/src).*\.(js|mjs|jsx|ts|tsx)$/));
-    console.log(`files: ${coverage.length}`);
-
-    const result =  coverage.map( entry => {
-      return { 
-        ...entry,
-        url: entry.url.replace(baseURL, process.cwd())
-      }
-    })
-    fs.writeFileSync(path.join(c8CLIOutput, `playwright_v8_coverage_${generateUUID()}.json`), JSON.stringify({result}));
-
-    //convert to istanbul format
+  context: async ({ context }, use) => {
+    await context.addInitScript(() =>
+      window.addEventListener('beforeunload', () =>
+        (window as any).collectIstanbulCoverage(JSON.stringify((window as any).__coverage__))
+      ),
+    );
     await fs.promises.mkdir(istanbulCLIOutput, { recursive: true });
-    let istanbulResult = {};
-    for (const entry of result) {
-      const converter = v8toIstanbul(entry.url, 0, { source: entry.source });
-      await converter.load();
-      converter.applyCoverage(entry.functions);
-      const converted = converter.toIstanbul();
-      istanbulResult = {...istanbulResult, ...converted};
+    await context.exposeFunction('collectIstanbulCoverage', (coverageJSON: string) => {
+      if (coverageJSON)
+        fs.writeFileSync(path.join(istanbulCLIOutput, `playwright_coverage_${generateUUID()}.json`), coverageJSON);
+    });
+    await use(context);
+    for (const page of context.pages()) {
+      await page.evaluate(() => (window as any).collectIstanbulCoverage(JSON.stringify((window as any).__coverage__)))
+      await page.close();
     }
-    fs.writeFileSync(path.join(istanbulCLIOutput, `playwright_coverage_${generateUUID()}.json`),  JSON.stringify(istanbulResult));
-  },
+  }
 });
 
 export const expect = test.expect;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+      react(),
+      istanbul({
+        include: 'src/*',
+        exclude: ['node_modules', 'test', 'coverage'],
+        extension: [ '.js', '.ts', 'tsx', 'jsx' ],
+        requireEnv: true
+      }),
+    ]
 })
